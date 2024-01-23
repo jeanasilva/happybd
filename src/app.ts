3e0b459995c24ccf83da949a2c57f6e759e24134
cnsgit <cnsgit000>
2024-01-23 16:28:01 -0400

    Update Project

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
 import express from 'express';
+import path from 'path';
 import userRoutes from './routes/UserRoutes';
 
 const app = express();
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 app.use(userRoutes);
 
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -9,7 +9,7 @@ export class UserController {
       const { name, email, birthday }: IUser = req.body;
       const newUser = await User.create({ name, email, birthday });
       res.status(201).json(newUser);
-     } catch (error) {
+    } catch (error) {
       if (error instanceof Error) {
         res.status(500).json({ error: error.message });
       } else {
@@ -22,6 +22,54 @@ export class UserController {
     try {
       const formattedUsers = await UserService.getAllFormattedUsers();
       res.status(200).json(formattedUsers);
+    } catch (error) {
+      if (error instanceof Error) {
+        res.status(500).json({ error: error.message });
+      } else {
+        res.status(500).json({ error: 'Ocorreu um erro desconhecido' });
+      }
+    }
+  }
+
+  public async updateUser(req: Request, res: Response): Promise<void> {
+    try {
+      const userId: number = parseInt(req.params.id);
+      const { name, email, birthday }: IUser = req.body;
+
+      const user = await User.findByPk(userId);
+
+      if (!user) {
+        res.status(404).json({ error: 'Usuário não encontrado' });
+        return;
+      }
+
+      user.name = name;
+      user.email = email;
+      user.birthday = birthday;
+      await user.save();
+
+      res.status(200).json(user);
+    } catch (error) {
+      if (error instanceof Error) {
+        res.status(500).json({ error: error.message });
+      } else {
+        res.status(500).json({ error: 'Ocorreu um erro desconhecido' });
+      }
+    }
+  }
+
+  public async deleteUser(req: Request, res: Response): Promise<void> {
+    try {
+      const userId: number = parseInt(req.params.id);
+      const user = await User.findByPk(userId);
+
+      if (!user) {
+        res.status(404).json({ error: 'Usuário não encontrado' });
+        return;
+      }
+
+      await user.destroy();
+      res.status(204).end();
     } catch (error) {
       if (error instanceof Error) {
         res.status(500).json({ error: error.message });
